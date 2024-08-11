3c6268c9bbed4e63f465d26b59c235dc034d448b
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-08-11 08:56:29 -0400

    Updated DB Info for Render.com 11

--- config/connection.js
+++ config/connection.js
@@ -2,22 +2,5 @@ const { verify } = require("jsonwebtoken");
 const { Sequelize } = require("sequelize");
 require('dotenv').config()
 
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.USERNAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: "postgres",
-  listen_addresses: "*",
-  dialectOptions: {
-    ssl: {
-      require: true, // This will help you. But you will see nwe error
-      rejectUnauthorized: false,
-      verify: "verify_none",
-      idle_interval: 60, 
-      idle_limit: 300 // This line will fix new error
-    }
-  },
-});
+const sequelize = new Sequelize("postgresql://cyrus:IExiEkUQQhV1H4lCgCZF0Y7c6ZeN8ovg@dpg-cqr06f5svqrc73fob4b0-a/techblog_51t7");
     module.exports = sequelize
