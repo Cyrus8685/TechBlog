1921f782de8a6d60fdc00d190530d054d656a40b
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-08-13 12:00:24 -0400

    added port number to DB URL 02

--- config/connection.js
+++ config/connection.js
@@ -2,5 +2,5 @@ const { verify } = require("jsonwebtoken");
 const { Sequelize } = require("sequelize");
 require('dotenv').config()
 
-const sequelize = new Sequelize("postgresql://cyrus:IExiEkUQQhV1H4lCgCZF0Y7c6ZeN8ovg@dpg-cqr06f5svqrc73fob4b0-a:10000/techblog_51t7");
+const sequelize = new Sequelize("postgresql://cyrus:IExiEkUQQhV1H4lCgCZF0Y7c6ZeN8ovg@dpg-cqr06f5svqrc73fob4b0-a:5432/techblog_51t7");
     module.exports = sequelize
