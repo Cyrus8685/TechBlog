ffa51b58ef52295ee1e8dce3ff995b042ac02e4e
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-08-13 11:54:26 -0400

    added port number to DB URL

--- views/public/javascript/signup.js
+++ views/public/javascript/signup.js
@@ -1,9 +1,14 @@
+const { use } = require("../../../controllers");
+
 async function signupFormHandler(event) {
     event.preventDefault();
 
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('username-signup').value.trim();
+    const password = document.querySelector('password-signup').value.trim();
+
+    console.log(username);
+    console.log(password);
 
     if (username && password) {
         const response = await fetch('/api/users', {
