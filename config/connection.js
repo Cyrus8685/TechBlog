293d4fa1258b4c21f6774489d71559ef9f29ab85
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-08-13 11:33:52 -0400

    Added .gitignore and correct DB URL

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,5 @@ const { verify } = require("jsonwebtoken");
 const { Sequelize } = require("sequelize");
 require('dotenv').config()
 
-const sequelize = new Sequelize("postgresql://cyrus:IExiEkUQQhV1H4lCgCZF0Y7c6ZeN8ovg@dpg-cqr06f5svqrc73fob4b0-a.oregon-postgres.render.com/techblog_51t7", {
-    ssl: true
-}
-);
+const sequelize = new Sequelize("postgresql://cyrus:IExiEkUQQhV1H4lCgCZF0Y7c6ZeN8ovg@dpg-cqr06f5svqrc73fob4b0-a/techblog_51t7");
     module.exports = sequelize
